8de201a5b12f3d01535a2f8fe52cf8f4771e33bd
Gaurav-spyder <g.guptabih@gmail.com>
2024-10-18 16:21:59 +0530

    MYSQL Task

---
+++ MYSQL work/writeSQLtoDB.java
@@ -0,0 +1,35 @@
+package sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class writeSQLtoDB {
+
+	public static void main(String[] args) throws SQLException {
+		String URL = "jdbc:mysql://localhost:3306/guvi";
+		String uname = "root";
+		String psw="Sija@123";
+
+		//creating connection to database
+		
+		Connection con= DriverManager.getConnection(URL, uname, psw);
+		
+		//create statement object to create statement
+		Statement s= con.createStatement();
+		String sql="insert into demo (empcode,empname,emppage,esalary) values"
+				+ "(101,'Jenny',25,10000),"
+				+ "(102,'Jacky',30,20000),"
+				+ "(103,'Joe',20,40000),"
+				+ "(104,'John',40,80000),"
+				+ "(105,'Shameer',25,90000)";
+		
+		
+		//Execute the query
+		 s.executeUpdate(sql);
+		 System.out.println("Data Inserted Successfully");
+	}
+
+}
